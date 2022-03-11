9c44a96fd3f35ce69c200738714a5e1b8a5d863c
tardes <curvatia2002@yahoo.es>
2022-03-11 17:25:11 +0100

    primer commit firebase

---
+++ Players.Android/FirebaseAuthService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using firebasesample.Droid.Services.FirebaseAuth;
+using firebasesample.Services.FirebaseAuth;
+using Players.Droid;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(FirebaseAuthService))]
+namespace firebasesample.Droid.Services.FirebaseAuth
+{
+    public class FirebaseAuthService : IFirebaseAuthService
+    {
+        public string getAuthKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUserSigned()
+        {
+            var user = Firebase.Auth.FirebaseAuth.GetInstance(MainActivity.app).CurrentUser;
+            var signedIn = user != null;
+            return signedIn;
+        }
+
+        public Task<bool> Logout()
+        {
+            try
+            {
+                Firebase.Auth.FirebaseAuth.GetInstance(MainActivity.app).SignOut();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public Task<bool> SignIn(string email, string password)
+        {
+            try
+            {
+
+                await Firebase.Auth.FirebaseAuth.GetInstance(MainActivity.app).SignInWithEmailAndPasswordAsync(email, password);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public void SignInWithGoogle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SignInWithGoogle(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SignUp(string email, string password)
+        {
+            try
+            {
+                await Firebase.Auth.FirebaseAuth.GetInstance(MainActivity.app).CreateUserWithEmailAndPasswordAsync(email, password);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
--- Players.Android/MainActivity.cs
+++ Players.Android/MainActivity.cs
@@ -4,25 +4,51 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Firebase;
 
 namespace Players.Droid
 {
     [Activity(Label = "Players", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static FirebaseApp app;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            InitFirebaseAuth();
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void InitFirebaseAuth()
+        {
+            var options = new FirebaseOptions.Builder()
+            .SetApplicationId("1:698104759680:android:39e692e6ae0408c0b42d65")
+            .SetApiKey("AIzaSyB1iwUiyj-QByHpm7jX2aZBcZU64C1am_8")
+            .Build();
+
+            if (app == null)
+                app = FirebaseApp.InitializeApp(this, options, "Players");
+
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (requestCode == FirebaseAuthService.REQ_AUTH && resultCode == Result.Ok)
+            {
+                GoogleSignInAccount sg = (GoogleSignInAccount)data.GetParcelableExtra("result");
+                MessagingCenter.Send(FirebaseAuthService.KEY_AUTH, FirebaseAuthService.KEY_AUTH, sg.IdToken);
+            }
         }
     }
 }
